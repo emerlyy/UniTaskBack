3416111ac4c512b7a42c81a5182d345290364271
Emerly <vadim200339@gmail.com>
2025-11-06 17:07:49 +0200

    create models

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,38 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { CoursesModule } from './courses/courses.module';
+import { TasksModule } from './tasks/tasks.module';
+import { SubmissionsModule } from './submissions/submissions.module';
+import { EvaluationModule } from './evaluation/evaluation.module';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST ?? 'localhost',
+      port: Number.parseInt(process.env.DB_PORT ?? '5432', 10),
+      username: process.env.DB_USER ?? 'postgres',
+      password: process.env.DB_PASSWORD ?? 'postgres',
+      database: process.env.DB_NAME ?? 'unitask',
+      synchronize: true,
+      autoLoadEntities: true,
+      uuidExtension: 'uuid-ossp',
+    }),
+    AuthModule,
+    UsersModule,
+    CoursesModule,
+    TasksModule,
+    SubmissionsModule,
+    EvaluationModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
